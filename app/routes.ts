d38100396dc3fa54a9df6fee27ebb00c5ec8cead
Melanie Burger <melanie.brgr@gmail.com>
2025-08-03 20:57:47 +0200

    feat: :sparkles: adds all routes

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,15 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home/home.tsx"),
+    // Front-End Web Development
+    route("gctavatar", "routes/frontendwebdevelopment/gctavatar/gctavatar.tsx"),
+    route("symptomsleuth", "routes/frontendwebdevelopment/symptomsleuth/symptomsleuth.tsx"),
+    route("shoppinglist", "routes/frontendwebdevelopment/shoppinglist/shoppinglist.tsx"),
+    route("cocktailsquiz", "routes/frontendwebdevelopment/cocktailsquiz/cocktailsquiz.tsx"),
+    route("lab3d", "routes/frontendwebdevelopment/lab3d/lab3d.tsx"),
+    // Design & Visual Communication
     route("apliaredesign", "routes/designandvisualcommmunication/apliaredesign/apliaredesign.tsx"),
+    route("naturecover", "routes/designandvisualcommmunication/naturecover/naturecover.tsx"),
+    // Other
+    route("writingandspeaking", "routes/writingandspeaking/writingandspeaking.tsx"),
 ] satisfies RouteConfig;
