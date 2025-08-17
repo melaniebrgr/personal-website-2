c5b77c6d96f14f23426d5932277c1d4a9b18762c
Melanie Burger <melanie.brgr@gmail.com>
2025-08-17 21:13:22 +0200

    fix: :speech_balloon: fixes BMCAA link

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <p>A few changes since my last update. This website was original built with Gulp, Angular 1 and Sass, and rebooting it after 3 years of working in React was both nostaligic and painful. I have a new appreciation for component driven development and perspective on the cascading part of CSS.</p>
         <p>Currently I'm a front-end developer at Rangle.io. I work on React and React Native projects from medical devices to e-commerce sites. I am interested in understanding how to create "pits of success" architecture that lead to robust and maintainable code. Over the past couple years I've also instructed and mentored for Bridgeschool.io a non-for-profit that skills up women breaking into tech. I currently live in Amsterdam, Netherlands 🇳🇱.</p>
         <p className="date">Aug 29, 2016</p>
-        <p>Welcome my portfolio website. I enjoy creating at the intersection of art, science, and technology, and organizing community events on behalf of the <a href="https://bmc.med.utoronto.ca/bmc/alumni/bmcaa/" target="_blank">BMCAA</a>. I live in Toronto, Canada 🇨🇦.</p>
+        <p>Welcome my portfolio website. I enjoy creating at the intersection of art, science, and technology, and organizing community events on behalf of the <a href="https://www.bmcaa.com/" target="_blank">BMCAA</a>. I live in Toronto, Canada 🇨🇦.</p>
       </article>
     </>
   );
